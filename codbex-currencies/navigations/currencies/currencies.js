6e629998dae9ff308a4df0a4c3c24cc725c9cfff
TIVMOF <tomyivanov06@gmail.com>
2024-12-03 13:23:01 +0000

    feat: New Dashboard Structure

--- codbex-currencies/navigations/currencies/currencies.js
+++ codbex-currencies/navigations/currencies/currencies.js
@@ -2,9 +2,8 @@ const navigationData = {
     id: 'currencies-navigation',
     label: "Currencies",
     view: "currencies",
-    group: "configurations",
-    orderNumber: 1000,
-    lazyLoad: true,
+    group: "reference-data",
+    orderNumber: 200,
     link: "/services/web/codbex-currencies/gen/codbex-currencies/ui/Currencies/index.html?embedded"
 };
 
