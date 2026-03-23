6817050bc12b52b96cabe93bd8434dfd15c2faa5
ThuF <jordan.p.pavlov@gmail.com>
2026-03-23 14:44:37 +0000

    Re-generate

--- codbex-currencies/gen/codbex-currencies/api/Settings/CurrencyController.ts
+++ codbex-currencies/gen/codbex-currencies/api/Settings/CurrencyController.ts
@@ -44,7 +44,7 @@ class CurrencyController {
             this.checkPermissions('write');
             this.validateEntity(entity);
             entity.Id = this.repository.create(entity) as any;
-            response.setHeader('Content-Location', '/services/ts/codbex-currencies/gen/codbex-currencies/api/Settings/CurrencyService.ts/' + entity.Id);
+            response.setHeader('Content-Location', '/services/ts/codbex-currencies/gen/codbex-currencies/api/Settings/CurrencyController.ts/' + entity.Id);
             response.setStatus(response.CREATED);
             return entity;
         } catch (error: any) {
--- codbex-currencies/gen/codbex-currencies/api/Settings/CurrencyRateController.ts
+++ codbex-currencies/gen/codbex-currencies/api/Settings/CurrencyRateController.ts
@@ -44,7 +44,7 @@ class CurrencyRateController {
             this.checkPermissions('write');
             this.validateEntity(entity);
             entity.Id = this.repository.create(entity) as any;
-            response.setHeader('Content-Location', '/services/ts/codbex-currencies/gen/codbex-currencies/api/Settings/CurrencyRateService.ts/' + entity.Id);
+            response.setHeader('Content-Location', '/services/ts/codbex-currencies/gen/codbex-currencies/api/Settings/CurrencyRateController.ts/' + entity.Id);
             response.setStatus(response.CREATED);
             return entity;
         } catch (error: any) {
